a0d63d58b7fc87a2caec83d95f35e24253ab65c5
HuaGuang <2475096613@qq.com>
2023-12-17 12:20:44 +0800

    First commit!

---
+++ src/main/java/top/xingzhexiaohui/jointqueryupdaterbackend/JointQueryUpdaterBackendApplication.java
@@ -0,0 +1,13 @@
+package top.xingzhexiaohui.jointqueryupdaterbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JointQueryUpdaterBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JointQueryUpdaterBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/xingzhexiaohui/jointqueryupdaterbackend/JointQueryUpdaterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package top.xingzhexiaohui.jointqueryupdaterbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JointQueryUpdaterBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
